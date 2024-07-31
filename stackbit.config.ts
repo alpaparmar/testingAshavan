1b2fd71810b58dd8605f367446c59a347f186d40
alpaparmar <alpaparmar1720@gmail.com>
2024-07-31 13:31:11 +0530

    Stackbit sitemap removed

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -25,32 +25,32 @@ const config = defineStackbitConfig({
         })
     ],
     // modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }],
-    siteMap: ({ documents, models }) => {
-        // console.info('models->>>>>>>>>>>>>>>>>', models);
-        const pageModels = models.filter(m => m.type === "page").map(m => m.name);
-        return documents.filter(d => pageModels.includes(d.modelName)).map(document => {
-            // console.info('document->>>>>>>>>>>>>>>>>>>>>', document);
-            // const slug = getLocalizedFieldForLocale(document.fields.slug);
-            // console.log('slug->>>>>>>>>>>>>>>>>>>>>', slug);
-            const resultData = extractPath(document.manageUrl)
-            // console.info('ammamamamaa', resultData, resultData.split('pages')[1].split('.md')[0].split('\\')[1]);
-            //@ts-ignore
-            // console.log('slug.manageURL', )
-            //@ts-ignore
-            // if (!slug || !slug.value) return null;
-            //@ts-ignore
-            // const urlPath = "/" + slug.value.replace(/^\/|\/$/g, '')
+    // siteMap: ({ documents, models }) => {
+    //     // console.info('models->>>>>>>>>>>>>>>>>', models);
+    //     const pageModels = models.filter(m => m.type === "page").map(m => m.name);
+    //     return documents.filter(d => pageModels.includes(d.modelName)).map(document => {
+    //         // console.info('document->>>>>>>>>>>>>>>>>>>>>', document);
+    //         // const slug = getLocalizedFieldForLocale(document.fields.slug);
+    //         // console.log('slug->>>>>>>>>>>>>>>>>>>>>', slug);
+    //         const resultData = extractPath(document.manageUrl)
+    //         // console.info('ammamamamaa', resultData, resultData.split('pages')[1].split('.md')[0].split('\\')[1]);
+    //         //@ts-ignore
+    //         // console.log('slug.manageURL', )
+    //         //@ts-ignore
+    //         // if (!slug || !slug.value) return null;
+    //         //@ts-ignore
+    //         // const urlPath = "/" + slug.value.replace(/^\/|\/$/g, '')
 
-            const finalResult = resultData && resultData?.replace('index', '')
-            console.info('finalresulrrrrr', finalResult)
-            return {
-                stableId: document?.srcProjectId,
-                urlPath: "/" + finalResult && finalResult?.replace('\\', '/'),
-                document,
-                isHomePage: resultData === "index"
-            };
-        }).filter(Boolean) as SiteMapEntry[];
-    },
+    //         const finalResult = resultData && resultData?.replace('index', '')
+    //         console.info('finalresulrrrrr', finalResult)
+    //         return {
+    //             stableId: document?.srcProjectId,
+    //             urlPath: "/" + finalResult && finalResult?.replace('\\', '/'),
+    //             document,
+    //             isHomePage: resultData === "index"
+    //         };
+    //     }).filter(Boolean) as SiteMapEntry[];
+    // },
     presetSource: {
         type: 'files',
         presetDirs: ['./.stackbit/presets']
